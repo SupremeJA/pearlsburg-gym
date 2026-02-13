99e8a75f54c24a07563f7c71533cc4c903fd3651
Joshua Afolabi <telvinamin15@gmail.com>
2026-02-13 05:52:05 +0100

    update

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -13,15 +13,22 @@ function Services() {
       <h2 className="text-2xl font-semibold font-poppins text-black mb-13">
         Services
       </h2>
-      <div className="flex justify-center items-center flex-col md:flex-row gap-10 text-balance md:px-20">
+      <div className="flex justify-center flex-col md:flex-row gap-10 text-balance md:px-20">
         {data.services.map((service, index) => {
           const IconComponent = iconMap[service.icon];
           if (!IconComponent) return null;
           return (
-            <div key={index}>
-              <IconComponent size={32} weight="thin" />
-              <h3 className="font-semibold">{service.title}</h3>
-              <p>{service.description}</p>
+            <div
+              key={index}
+              className="flex flex-col justify-center items-center gap-4 text-center"
+            >
+              <span className="rounded-full bg-burgundy text-white p-3">
+                <IconComponent size={32} weight="thin" />
+              </span>
+              <span>
+                <h3 className="font-semibold">{service.title}</h3>
+                <p>{service.description}</p>
+              </span>
             </div>
           );
         })}
